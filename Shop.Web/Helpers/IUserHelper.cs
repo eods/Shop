1dbad4669fff9e0dd2a8197ea806e2a9df4ea0a3
Eduardo De Santis <edesantis@live.com.ar>
2019-07-31 16:15:44 -0300

    repositorys

---
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Web.Helpers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Shop.Web.Data.Entities;
+
+    public interface IUserHelper
+    {
+        Task<User> GetUserByEmailAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+    }
+
+}
